e027f8f2224fea17a16a489ac8d7c79e3fe72898
talesCPV <tales@flexibus.com.br>
2024-11-20 15:20:48 -0300

    by_script

--- backend/asaas/webhook.php
+++ backend/asaas/webhook.php
@@ -3,6 +3,9 @@
 //    $exemplo = '{"id":"evt_05b708f961d739ea7eba7e4db318f621&7712248","event":"PAYMENT_CREATED","dateCreated":"2024-11-10 18:54:18","payment":{"object":"payment","id":"pay_6kmy0qdqpmk7vbte","dateCreated":"2024-11-10","customer":"cus_000006340074","paymentLink":null,"value":10,"netValue":9.01,"originalValue":null,"interestValue":null,"description":"teste webhook","billingType":"PIX","pixTransaction":null,"status":"PENDING","dueDate":"2024-11-13","originalDueDate":"2024-11-13","paymentDate":null,"clientPaymentDate":null,"installmentNumber":null,"invoiceUrl":"https://sandbox.asaas.com/i/6kmy0qdqpmk7vbte","invoiceNumber":"06949674","externalReference":null,"deleted":false,"anticipated":false,"anticipable":false,"creditDate":null,"estimatedCreditDate":null,"transactionReceiptUrl":null,"nossoNumero":null,"bankSlipUrl":null,"lastInvoiceViewedDate":null,"lastBankSlipViewedDate":null,"discount":{"value":0.00,"limitDate":null,"dueDateLimitDays":0,"type":"FIXED"},"fine":{"value":0.00,"type":"FIXED"},"interest":{"value":0.00,"type":"PERCENTAGE"},"postalService":false,"custody":null,"refunds":null}}';
 
     $post = file_get_contents('php://input');
+
+    saveFile($post);
+
     $json = json_decode($post);
 
     if($json->payment->externalReference == 'BACKHAND'){
@@ -23,7 +26,6 @@
             case 'PAYMENT_BANK_SLIP_VIEWED':
                 bankCkeckoutPayment($json);
                 break;
-    
         }
     }
 
@@ -83,6 +85,6 @@
         return $out;
     }
 
-//     http_response_code(200);
+     http_response_code(200);
 
 ?>
